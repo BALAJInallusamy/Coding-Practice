eab1708a2ab9fbd508a6d0f2dfaebe95ff15a34d
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-08 19:18:18 +0530

    Time: 40 ms (90.79%) | Memory: 10.5 MB (32.42%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-    
-    int rec(int n,vector<int> & dp){
-        if(n==0) return 0;
-        if(n==1 ) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=0) return dp[n];
-
-        int best=pow(n,0.5);
-        int ans=1e9;
-        for(int i=best;i>0;){
-            ans=min(ans,1+rec(n-i*i,dp));
-            i=pow(i*i-1,0.5);
-        }
-        return dp[n]=ans;
-    }
-
     int numSquares(int n) {
-        vector<int> dp(n+1,0);
-        for(int i=1;i<=pow(n,0.5);){
-            dp[i*i]=1;
-            i=pow(i+1,2);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
         }
-        return rec(n,dp);
-        
+
+        return dp[n];
     }
 };
-
-
