0f7b362a11cd297d068bca353e9bf2006080dcce
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-10 14:25:20 +0530

    Time: 4 ms (76.20%) | Memory: 7.3 MB (79.69%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -4,22 +4,19 @@ public:
         int left=0;
         int n=s.size(),ans=0;
         int i,j;
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++) dp[i][i]=1;
         ans+=n;
         for(left=0;left<n;left++){
             i=left-1;j=left+1;
             while(i>-1 && j<n){
-                if(dp[i+1][j-1] && s[i]==s[j]) ans++,dp[i][j]++,i--,j++;
+                if(s[i]==s[j]) ans++,i--,j++;
                 else break;
             }
             i=left;j=left+1;
             if(s[i]==s[j]){
-                dp[i][j]++;
                 ans++;
                 i=left-1;j=left+2;
                 while(i>-1 && j<n){
-                    if(dp[i+1][j-1] && s[i]==s[j]) ans++,dp[i][j]++,i--,j++;
+                    if( s[i]==s[j]) ans++,i--,j++;
                     else break;
                 }
             }
@@ -27,8 +24,6 @@ public:
         return ans;
     }
 };
- //a a b b c c c c b b a  a
- //0 1 2 3 4 5 6 7 8 9 10 11
       
 
 
