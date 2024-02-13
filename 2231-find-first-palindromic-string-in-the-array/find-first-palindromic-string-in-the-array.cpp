c0398702c3b1db3d60865d86c3223945c207f00b
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-13 18:13:04 +0530

    Time: 59 ms (16.26%) | Memory: 26.7 MB (16.60%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool rec(string s){
-        int r=s.size()-1;
-        int l=0;
-        while(l<=r){ if(s[l]==s[r]) l++,r--;
-        else return false;}
-        return true;
-    }
-    string firstPalindrome(vector<string>& words) {
-        for(int i=0;i<words.size();i++){
-            if(rec(words[i])) return words[i];
-        }
-        return "";
-    }
+bool rec(string s){
+int r=s.size()-1;
+int l=0;
+while(l<=r){ if(s[l]==s[r]) l++,r--;
+else return false;}
+return true;
+}
+string firstPalindrome(vector<string>& words) {
+for(int i=0;i<words.size();i++){
+if(rec(words[i])) return words[i];
+}
+return "";
+}
 };
