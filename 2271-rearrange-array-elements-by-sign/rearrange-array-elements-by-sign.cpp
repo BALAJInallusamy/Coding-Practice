4752c1dfdc733f955172aa5743113bcd40180260
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-14 17:22:14 +0530

    Time: 166 ms (34.01%) | Memory: 136.8 MB (23.54%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
-        for(auto it:nums){
-            if(it>0) pos.push_back(it);
-            else neg.push_back(it);
-        }
-        for(int i=0,j=0,k=0;i<nums.size();i++){
-            i%2==0?nums[i]=pos[j++]:nums[i]=neg[k++];
-        }
-        return nums;
-    }
+vector<int> rearrangeArray(vector<int>& nums) {
+vector<int> pos;
+vector<int> neg;
+for(auto it:nums){
+if(it>0) pos.push_back(it);
+else neg.push_back(it);
+}
+for(int i=0,j=0,k=0;i<nums.size();i++){
+i%2==0?nums[i]=pos[j++]:nums[i]=neg[k++];
+}
+return nums;
+}
 };
