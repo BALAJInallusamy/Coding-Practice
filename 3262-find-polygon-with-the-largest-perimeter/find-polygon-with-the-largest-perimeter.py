91540f17d516ace11ac76a16cc7cc17a43037715
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-15 13:41:53 +0530

    Time: 532 ms (56.77%) | Memory: 31.6 MB (91.17%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -8,16 +8,12 @@ class Solution:
                 pre.append(nums[i]+pre[i-1])
             else:
                 pre.append(nums[i])
-        #5 5  5
-        #5 10 15
         for j in range(1,n):
             if nums[-j]<pre[-j-1]:
                 if j<n-1:
                     return pre[-j]
                 else:
                     return -1
-            else:
-                continue
         return -1
         
 
