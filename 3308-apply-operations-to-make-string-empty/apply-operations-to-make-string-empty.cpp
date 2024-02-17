bf2bd06be665b014a3e6ad54f26de4d2e28f8762
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-02-17 23:06:04 +0530

    Time: 300 ms (25.00%) | Memory: 31.9 MB (25.00%) - LeetSync

--- 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     string lastNonEmptyString(string s) {
-        //aabcbbca
-        //a3 b3 c2
         unordered_map<char,int> mp;
         int maxi=0;
         for(auto it:s) mp[it]++,maxi=max(mp[it],maxi);
