a4272104bad55a6531f4a6ee2cf6911c1e37aaf1
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-03-08 22:39:47 +0530

    Time: 46 ms (41.26%) | Memory: 16.5 MB (97.35%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -3,7 +3,7 @@ class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         ans=0
         maxi=1
-        a = [0]*100
+        a = [0 for x in range(100)]
         for x in nums: 
             a[x-1]+=1
             if a[x-1]==maxi:
