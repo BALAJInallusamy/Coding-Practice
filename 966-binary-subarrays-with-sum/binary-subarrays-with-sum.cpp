4d262c1dfd9baeff67655f2e455cac7b24e88f5e
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-03-14 12:12:46 +0530

    Time: 51 ms (28.85%) | Memory: 39.9 MB (15.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -3,7 +3,6 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         unordered_map<int,int> mp;
         int n= nums.size(), ans=0, sum=0;
-        
         for(int i=0; i< n; i++){
             sum+=nums[i];
             if(sum==goal) ans++;
