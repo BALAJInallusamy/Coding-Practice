da2051286cf28e4719c06f16d700ffc697f208b3
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-05-28 21:27:02 +0530

    Time: 247 ms (79.08%) | Memory: 48.8 MB (94.20%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int counter(int i, int mid, int j,vector<int>& nums){
         int count=0;
-        int left = i;
+        int left=i;
+        
         for(int r=mid+1;r<=j;r++){
-            while(left <= mid && nums[left] <= 2*1LL*nums[r]){
-                left++;
-            }
-            if(left == mid + 1) break; 
-            if(nums[left] > 2*1LL*nums[r]){
-                count += mid - left + 1;
-            }
+            
+            while(left<=mid && nums[left]<=2*1LL*nums[r]) left++;
+            count+=mid-left+1;
         }
         sort(nums.begin()+i,nums.begin()+j+1);
         return count;
@@ -25,3 +22,7 @@ public:
         return rec(0,nums.size()-1,nums);
     }
 };
+
+//  12 23 31 51
+//  1223  1135
+//
