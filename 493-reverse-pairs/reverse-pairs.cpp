c0dc8929a0efe30199c46597cafa55213f11f4b4
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-05-28 21:25:39 +0530

    Time: 251 ms (77.18%) | Memory: 48.6 MB (95.20%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int counter(int i, int mid, int j,vector<int>& nums){
+        int count=0;
+        int left = i;
+        for(int r=mid+1;r<=j;r++){
+            while(left <= mid && nums[left] <= 2*1LL*nums[r]){
+                left++;
+            }
+            if(left == mid + 1) break; 
+            if(nums[left] > 2*1LL*nums[r]){
+                count += mid - left + 1;
+            }
+        }
+        sort(nums.begin()+i,nums.begin()+j+1);
+        return count;
+    }
+    int rec(int i, int j, vector<int> &nums){
+        
+        int mid=(j+i)/2;
+        if(i>=j) return 0;
+        return rec(i,mid,nums)+ rec(mid+1,j,nums)+ counter(i,mid,j,nums);
+    }
+    int reversePairs(vector<int>& nums) {
+        return rec(0,nums.size()-1,nums);
+    }
+};
