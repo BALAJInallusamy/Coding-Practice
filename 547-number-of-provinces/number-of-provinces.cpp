4b4b62017c413771e3a96d347e91b59f353e4d53
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-06-20 02:21:01 +0530

    Time: 38 ms (6.78%) | Memory: 20 MB (14.38%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& iscon) {
+        int n=iscon.size();
+        vector<vector<int>> graph(n );
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(iscon[i][j]) graph[i].push_back(j),graph[j].push_back(i);
+            }
+        }
+        stack<int> st;
+        vector<int> vis(n);
+        int ans=0;
+        for(int i=0;i<n;i++){
+
+            if(vis[i]==0){
+                vis[i]=1;
+                ans++;
+                st.push(i);
+                while(!st.empty()){
+                    int temp=st.top();
+                    st.pop();
+                    vis[temp]=1;
+                    for(auto it:graph[temp]){
+                        if(vis[it]==0) st.push(it);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
