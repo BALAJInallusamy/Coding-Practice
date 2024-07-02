caa9b01e4c415c368012e647b1658407784793d6
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-07-03 01:58:56 +0530

    Time: 273 ms (5.00%) | Memory: 10.3 MB (8.41%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    int rec(int start, int n, vector<vector<int>>& split, vector<vector<int>>& dp, int k){
+    int rec(int start, int n, vector<int>& split, vector<vector<int>>& dp, int k){
 
         if(start>=n ) return -1e9;
-        if(k==0) return split[start][n-1];
+        if(k==0) return start>0 ? split[n-1] - split[start-1]: split[n-1] ;
 
         if(dp[start][k]!=-1) return dp[start][k];
 
         int ans=INT_MAX;
         for(int i=start; i<n; i++){
-            
+            int pre= split[i];
+            if(start>0) pre-= split[start-1];
             int next= rec(i+1,n,split,dp,k-1);
-            int cur= max( split[start][i], next );
+            int cur= max( pre, next );
             ans = min(ans , cur);
         }
         return dp[start][k]=ans;
     }
     int splitArray(vector<int>& nums, int k) {
         int n= nums.size();
-        vector<vector<int>> split(n,vector<int> (n));
+        vector<int> split(n,nums[0]);
 
-        for(int i=0;i<n;i++){
-            split[i][i]=nums[i];
-            for(int j=i+1;j<n;j++){
-                split[i][j] = nums[j] + split[i][j-1];
-            }
+        for(int i=1;i<n;i++){
+            split[i]=nums[i]+split[i-1];
+            
         }
-        vector<vector<int>> dp(n,vector<int>(k,-1));
-
-
-        
+        vector<vector<int>> dp(n,vector<int>(k,-1));  
        int ans = rec(0,n,split,dp,k-1);
-
-        for(auto it:dp){
-            for(auto x:it){
-                cout<<x<<" ";
-            }
-            cout<<endl;
-        }
         return ans;
         
     }
