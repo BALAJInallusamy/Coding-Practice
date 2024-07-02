b72ec43b1dc880f184c033ffc66d91475a48845b
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-07-03 00:53:28 +0530

    Time: 171 ms (87.06%) | Memory: 257.9 MB (76.08%) - LeetSync

--- 2633-minimum-cost-to-split-an-array/minimum-cost-to-split-an-array.cpp
+++ 2633-minimum-cost-to-split-an-array/minimum-cost-to-split-an-array.cpp
@@ -1,31 +1,34 @@
 class Solution {
- public:
-  int minCost(vector<int>& nums, int k) {
-    constexpr int kMax = 1001;
-    const int n = nums.size();
-    // trimmedLength[i][j] := trimmed(nums[i..j]).length
-    vector<vector<int>> trimmedLength(n, vector<int>(n));
-    // dp[i] := min cost to split nums[i:]
-    vector<int> dp(n + 1, INT_MAX / 2);
+public:
+    int minCost(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<int>> prefix(n + 1, vector<int>(n, 0));
+        for (int i = 1; i <= n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                prefix[i][j] = prefix[i - 1][j];
+            }
+            prefix[i][nums[i - 1]] += 1;
+        }
+        // for(auto it:prefix){
+        //     for(auto x:it) cout<<x<<" ";cout<<endl;
+        // }
+        vector<int> dp(n + 1, 0);
 
-    for (int i = 0; i < n; ++i) {
-      int length = 0;
-      vector<int> count(kMax);
-      for (int j = i; j < n; ++j) {
-        if (++count[nums[j]] == 2)
-          length += 2;
-        else if (count[nums[j]] > 2)
-          ++length;
-        trimmedLength[i][j] = length;
-      }
+        for (int i = 1; i <= n; i += 1) {
+            // printf("i: %d\n", i);
+            vector<int> cnts = prefix[i];
+            int once = 0; 
+            for (int c: cnts) {
+                if (c == 1) once += 1;
+            }
+            dp[i] = INT_MAX;
+            for (int j = 0; j < i; j += 1) {
+                dp[i] = min(dp[i], dp[j] + (i - j) - once + k);
+                cnts[nums[j]] -= 1;
+                if (cnts[nums[j]] == 1) once += 1;
+                else if (cnts[nums[j]] == 0) once -= 1;
+            }
+        }
+        return dp[n];
     }
-
-    dp[n] = 0;
-
-    for (int i = n - 1; i >= 0; --i)
-      for (int j = i; j < n; ++j)
-        dp[i] = min(dp[i], k + trimmedLength[i][j] + dp[j + 1]);
-
-    return dp[0];
-  }
 };
