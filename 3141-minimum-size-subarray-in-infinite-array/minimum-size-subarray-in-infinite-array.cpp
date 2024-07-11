2020ea44e602cc8c710567b68d2a1753e257d211
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-07-11 13:12:46 +0530

    Time: 250 ms (14.79%) | Memory: 123.6 MB (11.46%) - LeetSync

--- 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.cpp
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.cpp
@@ -9,9 +9,9 @@ public:
         unordered_map<long, int> dp{{0L, -1}};
         for (int i = 0; i < 2 * n; ++i) {
             su += A[i % n];
+            dp[su] = i;
             if (dp.count(su - target))
                 res = min(res, i - dp[su - target]);
-            dp[su] = i;
         }
         return res < n ? res + k * n : -1;
     }
