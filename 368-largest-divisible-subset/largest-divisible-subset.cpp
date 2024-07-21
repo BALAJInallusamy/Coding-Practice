17af5b39993256134a5d20ecbb1dfbfa5e41ad47
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-07-22 02:30:54 +0530

    Time: 41 ms (5.57%) | Memory: 18.3 MB (5.09%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        if(nums.size()==0){
-            return nums;
-        }
         sort(nums.begin(),nums.end());
-        int flag=1;
-        if(nums[0]==1){
-            flag=0;
-        }
-        else{
-            nums.push_back(1);
-            flag=1;
-        }
-        sort(nums.begin(),nums.end());
-        int i,j;
-        int dp[nums.size()][2];
-        for(i=nums.size()-1;i>=0;i--){
-            dp[i][0]=0;
-            dp[i][1]=i;
-            for(j=i+1;j<nums.size();j++){
-                if((nums[j]%nums[i])==0){
-                    if(dp[j][0]>dp[i][0]){
-                        dp[i][0]=dp[j][0];
-                        dp[i][1]=j;
-                    }  
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (2));
+        for(int i = n-1 ; i>=0 ; i--){
+            dp[i] = {i,0};
+            for(int j= i+1; j<n ; j++){
+                if(nums[j] % nums[i] == 0) {
+                    vector<int> temp = { j, dp[j][1] };
+                    dp[i] = dp[i][1] < dp[j][1] ? temp :  dp[i];
                 }
             }
-            dp[i][0]++;
+            dp[i][1]++;
         }
-        i=0;
-        vector<int> t;
-        t.push_back(nums[i]);
-        while(dp[i][1]!=i){
-            i=dp[i][1];
-            t.push_back(nums[i]);
-            
+        vector<int> ans;
+        int start=0;
+        for(int i=0;i<n;i++){
+            start = dp[start][1] < dp[i][1] ? i:start;
         }
-        if(flag==1){
-            t.erase(t.begin());
+        while(dp[start][0] != start){
+            ans.push_back(nums[start]);
+            start = dp[start][0];
         }
-        return t;
-        
+        ans.push_back(nums[start]);
+        //for(auto it:dp) cout<<it[0]<<","<<it[1]<<"  ";
+        return ans;
     }
 };
