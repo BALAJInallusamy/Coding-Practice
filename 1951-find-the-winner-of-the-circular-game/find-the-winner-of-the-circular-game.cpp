dc6139f162bd109c3d0eaf4b076fbca23ae24859
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-09-03 06:59:59 +0530

    Time: 4 ms (50.59%) | Memory: 7.9 MB (27.69%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> s(n);
+        iota(s.begin(),s.end(),1);
+        int last = 0;
+        k--;
+        while(s.size()>1){
+            int ind = (last+k)%(s.size());
+            cout<<s[ind]<<" ";
+            s.erase(s.begin()+ind);
+            last = ind;
+        }
+        return s[0];
+    }
+};
