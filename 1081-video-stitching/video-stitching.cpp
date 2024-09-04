1e0fb752f618760089691e81a4162db3d7378b45
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-09-05 00:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (86.62%) - LeetSync

--- 1081-video-stitching/video-stitching.cpp
+++ 1081-video-stitching/video-stitching.cpp
@@ -2,18 +2,27 @@ class Solution {
 public:
     int videoStitching(vector<vector<int>>& clips, int time) {
         int n = clips.size();
-        vector<int> dp(time+1, n + 1);
+        vector<int> dp(time+1,0);
         sort(clips.begin(),clips.end());
-        dp[0] = 0;
-        for(int i = 0 ; i<n ; i ++){
-            for(int j = clips[i][0] +1 ; j<= min(clips[i][1],time) ; j++){
-                dp[j] = min( dp[j] , dp[max(0,clips[i][0])] + 1);
-            }
+
+        for(int i = 0 ; i <n ; i ++){
+            int start = min(clips[i][0],time);
+            dp[start] = max(dp[start], min(clips[i][1],time));
         }
-        // for(auto it:dp) cout<<it<<" ";
-        return dp[time] == n+1?-1:dp[time];
+        int end = 0 , far = 0, count = 0;
+        for(int i = 0 ; i< time+1 ; i++){
+            if(end<i){
+                if(far<=end) return -1;
+                end = far;
+                count++;
+            }
+            far = max(far, dp[i]);
+        }
+        return count;
     }
 };
 
 
-// 0 2  
+// 02 15 19 46 59 810
+// 0 1 2 3 4 5 6 7 8 9 10
+// 0 0
