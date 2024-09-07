d2279bb6f566d1abba3ee9733108306eba9f8e96
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2024-09-07 16:19:37 +0530

    Time: 10 ms (37.21%) | Memory: 25.8 MB (62.21%) - LeetSync

--- 3025-minimum-operations-to-form-subsequence-with-target-sum/minimum-operations-to-form-subsequence-with-target-sum.cpp
+++ 3025-minimum-operations-to-form-subsequence-with-target-sum/minimum-operations-to-form-subsequence-with-target-sum.cpp
@@ -1,27 +1,63 @@
 class Solution {
 public:
     int minOperations(vector<int>& A, int target) {
-        long long tot = accumulate(A.begin(), A.end(), 0L), res = 0;
-        if (tot < target)
-            return -1;
-        sort(A.begin(), A.end());
-        while (target > 0) {
-            int a = A.back();
-            A.pop_back();
-            if (tot - a >= target) {
-                tot -= a;
-            } else if (a <= target) {
-                tot -= a;
-                target -= a;
-            } else {
-                A.push_back(a / 2);
-                A.push_back(a / 2);
-                res += 1;
-            }
+        vector<int> count(32,0);
+        int n = A.size();
+        int m = 0;
+        long long s = 0;
+        for(int i = 0 ; i< n ; i++){
+            count[log2(A[i])]++;
+            s+= A[i];
         }
-        return res;
+        if(s<target) return -1;
+        for(int i =0 ; i< 32 ; i++){
+            if(count[i]) m = i;
+        }
+
+        int ans = 0;
+
+        for(int i = 0 ; i< m+1; i++){
+
+            // for(auto it: count) cout<<it<<" ";
+            // cout<<endl;
+
+            if((target & (1<<i)) != 0 ){
+
+                if( count[i] > 0 ){
+                    count[i]--;
+                }
+                else{
+                    for(int j = i+1 ; j< m+1 ; j++){
+                        ans++;
+                        if(count[j] != 0){ 
+                            count[j]--; 
+                            break;
+                        }
+                        else{
+                            count[j] = 1;
+                        }
+                    }
+                }
+            }
+            count[i+1] += count[i]/2;
+            count[i] -= ((int)(count[i]/2))*2;
+        }
+        return ans;
     }
 };
-// 12 -> 110
-// 
+//       368421
+// 12 -> 001100
+// 1 2 4 8 16 32
+// 2 1 0 0 0 1
+// 0 3 0 0 0 1
+// 0 1 1 0 0 1
+// 0 1 1 0 2 0
+// 0 1 1 2 1 0
+
+// 1 1 2 16 16
+// 1 1 2 8 8 16
+
 // 12 -> 4 4 4 , 1 1 2
+
+// 100011
+// 0= 1 1 =1 32 1
