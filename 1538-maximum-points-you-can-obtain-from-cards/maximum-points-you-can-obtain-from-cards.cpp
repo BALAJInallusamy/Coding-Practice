df4528309dd2335dc3ab3bfabd5296fee09a49c0
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2025-01-27 03:19:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (46.94%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int n= cardPoints.size(), ans=0;
-        int count=0, total=cardPoints[0];
-        for(int i=1;i<n;i++){
-            total+=cardPoints[i];
-            cardPoints[i]+=cardPoints[i-1];
+    int maxScore(vector<int>& nums, int k) {
+        int sum = 0;
+        for(auto it:nums) sum+=it;
+        int n = nums.size();
+        int temp = 0;
+        for(int i = 0 ; i<n- k ; i++){
+            temp+= nums[i];
         }
-        if(n==k) return total;
-        for(int i=0;i<= k;i++){
-            int sub= 0;
-            if(i) sub=cardPoints[i+n -k-1]-cardPoints[i-1];
-            else sub=cardPoints[i+n-k-1];
-            ans=max(ans,total- sub);
+        int ans = temp;
+        for(int i=0;i<k ; i++){
+            temp+= -nums[i] + nums[n-k+i];
+            ans = min(temp,ans);
         }
-        return ans;
+        return sum-ans;
     }
 };
-
-// nums[l] + rec(l+1,r,k-1)
-// nums[r] + rec(l,r-1,k-1)
-// dp[l][r]= max(left,right)
