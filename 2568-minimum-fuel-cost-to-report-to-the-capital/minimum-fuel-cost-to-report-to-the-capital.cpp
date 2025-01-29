7aef9065532e3e0f744efc2a1bb3d5286c3455df
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2025-01-30 00:23:46 +0530

    Time: 648 ms (9.86%) | Memory: 236.1 MB (6.11%) - LeetSync

--- 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    long long ans = 0; int s;
-    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        vector<vector<int>> graph(roads.size() + 1); s = seats;
-        for (vector<int>& r: roads) {
-            graph[r[0]].push_back(r[1]);
-            graph[r[1]].push_back(r[0]);
+    vector<long long> rec(int node ,int parent, vector<vector<int>>& path, int seat){
+        if( path[node].size()==1 ){
+            return {1,1};
         }
-        dfs(0, 0, graph);
+        long long count = 1 , fuel = 0;
+        for(auto it:path[node]){
+            if(it==parent) continue;
+            vector<long long> res = rec(it,node,path, seat);
+            count+= res[0];
+            fuel += res[1];
+        }
+        fuel+= count/seat ;
+        fuel+= (count%seat!=0);
+        return {count, fuel};
+    }
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size() + 1;
+        vector<vector<int>> path(n);
+        for(auto it: roads){
+            path[it[0]].push_back(it[1]);
+            path[it[1]].push_back(it[0]);
+        }
+        long long ans = 0;
+        for(auto it:path[0]){
+        ans += rec(it,0,path,seats)[1];
+        }
         return ans;
     }
-    int dfs(int i, int prev, vector<vector<int>>& graph, int people = 1) {
-        for (int& x: graph[i]) {
-            if (x == prev) continue;
-            people += dfs(x, i, graph);
-        }
-        if (i != 0) ans += (people - 1) / s + 1;
-        return people;
-    }    
 };
-
-// 1,1,1
-// 2,3,2
-
-// 0,3,2
-// 0,3,3
-
-// 
