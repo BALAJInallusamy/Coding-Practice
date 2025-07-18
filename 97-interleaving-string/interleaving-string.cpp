1842b8c4c3396fa3bff358295b47df93cdba273b
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2025-07-18 14:06:03 +0530

    Time: 25 ms (20.16%) | Memory: 34.2 MB (14.39%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    
-    int dp[101][101][201];
-    bool rec(int i , int j , int k , int n , int m , string s1, string s2, string s3 ){
-        if(i==n && j==m && k == n+m) return true;
-        bool ans = 0;
-        if(dp[i][j][k]!= -1) return dp[i][j][k];
-        if(i == n){
-            return s2.substr(j) == s3.substr(k);
-        }
-        else if(j == m){
-            return s1.substr(i) == s3.substr(k);
-        }
-        if(s1[i] == s3[k]) {
-            ans|= rec(i+1,j, k+1 , n , m, s1, s2, s3);
-        }
-        if(s2[j] == s3[k]){
-            ans|= rec(i,j+1, k+1 , n , m, s1, s2, s3);
-        }
-        return dp[i][j][k] = ans;
+    bool rec(int i, int j,int k, int n, int m, string s1, string s2, string s3,vector<vector<int>>& dp){
+        if(i==n && j==m) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        bool one = 0, two = 0;
+        if(i<n && s1[i]==s3[k]) one|= rec(i+1,j,k+1,n,m,s1,s2,s3,dp);
+        if(j<m && s2[j]==s3[k]) two|= rec(i,j+1,k+1,n,m,s1,s2,s3,dp);
+        if(i<n && j<m && s1[i]!= s3[k] && s2[j]!=s3[k]) return 0;
+        return dp[i][j] = one||two;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size() , m =s2.size() ;
-        if(n+m != s3.size()) return false;
-        memset(dp,-1,sizeof(dp));
-
-        return rec(0,0,0,n,m,s1,s2,s3);
+        int n = s1.size(), m=s2.size();
+        if(n+m!= s3.size()) return 0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return rec(0,0,0,n,m,s1,s2,s3,dp);
     }
 };
