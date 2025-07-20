ccb30a51557055ceea8027c92d1a5c4e2e147235
BALAJInallusamy <121440466+BALAJInallusamy@users.noreply.github.com>
2025-07-20 10:43:55 +0530

    Time: 107 ms (5.55%) | Memory: 62 MB (11.29%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void rec(int i, vector<int>& nums, int n, int tar , vector<int> ds){
-        if(tar <=0 ) {
+    void rec(int i, int n, vector<int>& nums, vector<int> ds, int tar){
+        if(tar<0) return;
+        if(i==n) return;
+        if(tar==nums[i]) {
+            ds.push_back(nums[i]);
             ans.push_back(ds);
             return;
         }
-        if(i == n) return;
-        rec(i+1, nums, n , tar, ds);
-        if(nums[i]<= tar) {
-            ds.push_back(nums[i]);
-            rec(i, nums,n, tar - nums[i] , ds);
-        }
+        rec(i+1,n,nums,ds,tar);
+        ds.push_back(nums[i]);
+        rec(i,n,nums,ds,tar-nums[i]);
     }
-    vector<vector<int>> combinationSum(vector<int>& nums, int tar) {
-        int n = nums.size();
-        rec(0,nums,n,tar,{});
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        rec(0,nums.size(),nums,{},target);
         return ans;
     }
 };
